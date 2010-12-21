235e5e5083f5ca6bbfa0b4e0dc883f387fd1f4bd
Christian Reber <christian@6wunderkinder.com>
2010-12-22 00:23:26 +0100

    - Printing finished - Cloudapp nearly final

--- js/helpers/html.js
+++ js/helpers/html.js
@@ -48,7 +48,7 @@ generateListContentHTML = function(list_id, list_name)
 {
 	var html = '';
 
-	html +=  "<div id='listfunctions'><a class='list-print'></a><a class='list-email'></a><a class='list-cloud'></a><div id='cloudtip'><span>copy</span> url goes here</div></div>";
+	html +=  "<div id='listfunctions'><a class='list-print'></a><a class='list-email'></a><a class='list-cloud'></a><div id='cloudtip'><span class='link'>http://wunderli.st/812ße912a09k</span>&nbsp;&nbsp;<span class='copy'> copy </span></div></div>";
 	html += "<h1>" + unescape(list_name) + "</h1>";
 	html += "<div class='add'>";
 	html += "<input type='text' class='input-add' placeholder='" + language.data.add_task + "' />";
