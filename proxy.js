22a49bd5098d29a6d71ec2ee0c68da0aa9b44e61
Aditya <aditya@netroy.in>
2011-11-01 05:46:55 +0530

    added step2 mock to proxy

--- proxy.js
+++ proxy.js
@@ -2,10 +2,12 @@
 
 var connect = require('connect'),
     app = connect.createServer(),
+     fs = require('fs'),
     port = process.env.StaticPort || 8888;
 
-var syncData = require('fs').readFileSync(__dirname + "/sync.json");
+var syncData = ['', fs.readFileSync(__dirname + "/step1.json"), fs.readFileSync(__dirname + "/step2.json")];
 
+app.use(connect.bodyParser());
 app.use(connect.static(process.cwd()));
 app.use(connect.router(function(router){
   router.post("/login", function(req, resp){
@@ -15,7 +17,7 @@ app.use(connect.router(function(router){
   });
   router.post("/1.2.0", function(req, resp){
     resp.writeHead(200, "Content-type: text/json");
-    resp.write(syncData);
+    resp.write(syncData[req.body.step || 0]);
     resp.end();
   });
 }));
