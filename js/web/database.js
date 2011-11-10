4f9d84ae5d150046dbc1f3fd260443e9f501bea5
Aditya <aditya@netroy.in>
2011-11-10 07:05:22 +0530

    database.js should implement all 4 engines - IndexedDB, WebSQL, AJAX & Titanium

--- js/web/database.js
+++ js/web/database.js
@@ -7,6 +7,13 @@ wunderlist.database = (function(wunderlist, html, global, undefined){
       DB_BYTES = 5*1024*1024, // 5MB
       db;
 
+  var ENGINES = {
+    IXDB: 0,  // IndexedDB - For Firefox & all upcoming browsers.
+    WSQL: 1,  // WebSQL - For existing WebKit (desktop & mobile) & Opera.
+    AJAX: 2,  // AJAX (over the wire) - for ancient browsers.
+    TAPP: 3   // Titanium Appecelarator - Native apps for Devices.
+  };
+
   var log = function(){
     console.log.apply(console, arguments);
   };
