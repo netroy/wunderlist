937a5406165ecc001a0dec630decce1aa6e24a33
Aditya <aditya@netroy.in>
2011-11-15 02:46:04 +0530

    proxy files names were mixed up.. fixed now

--- bin/proxy.js
+++ bin/proxy.js
@@ -1,29 +1,52 @@
 #!/usr/bin/env node
 
 var connect = require('connect'),
-    app = module.exports = connect.createServer(),
-    fs = require('fs'),
-    port = 8888;
+        app = module.exports = connect.createServer(),
+         fs = require('fs'),
+       http = require('http'),
+       port = 8888;
 
-var syncData = ['', 
-                fs.readFileSync(__dirname + "/data/step1.json"), 
-                fs.readFileSync(__dirname + "/data/step2.json")
-               ];
 var versionData = fs.readFileSync(__dirname + "/data/version.txt");
 
-app.use(connect.bodyParser());
+var postOptions = {
+  host: 'sync.wunderlist.net',
+  port: '80',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Host': 'sync.wunderlist.net',
+    'Origin': 'http://sync.wunderlist.net',
+    'Referer': 'http://www.wunderlist.net/home'
+  }
+};
+
 app.use(connect.static(process.cwd()));
 app.use(connect.router(function(router){
-  router.post("/login", function(req, resp){
-    resp.writeHead(200, "Content-type: text/json");
-    resp.write('{"code": 200,"user_id": "1140994"}');
-    resp.end();
+  router.post(/^\/(register|login|password|invite|(edit|delete)-account|1\.2\.0)\/?$/, function(req, resp){
+    var body = '';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk) {
+      body += chunk;
+    });
+    req.on('end', function(){
+      postOptions.path = req.url;
+      postOptions.headers['Content-Length'] = body.length;
+      var postRequest = http.request(postOptions, function(response) {
+        response.headers['Content-Type'] = 'application/json';
+        resp.writeHead(response.statusCode, response.headers);
+        response.setEncoding('utf8');
+        response.on('data', function (chunk) {
+          resp.write(chunk);
+        });
+        response.on('end', function () {
+          resp.end();
+        });
+      });
+      postRequest.write(body);
+      postRequest.end();
+    });
   });
-  router.post("/1.2.0", function(req, resp){
-    resp.writeHead(200, "Content-type: text/json");
-    resp.write(syncData[req.body.step || 0]);
-    resp.end();
-  });
+
   router.get("/version.txt", function(req, resp){
     resp.writeHead(200, "Content-type: text/json");
     resp.write(versionData);
