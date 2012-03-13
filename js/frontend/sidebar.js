5b876188e4ba1cf83abf2483ded0e8d3993ef4d5
Aditya Y <aditya@netroy.in>
2012-03-13 22:32:56 +0530

    sidebar layout done

--- js/frontend/sidebar.js
+++ js/frontend/sidebar.js
@@ -1,15 +1,76 @@
 /*global console: false*/
-define('frontend/sidebar', ['libs/jquery'], function($, undefined) {
-  "use strict";
+define('frontend/sidebar', ['libs/jquery', 'helpers/settings'], function($, settings, undefined) {
 
-  var self = {};
+  'use strict';
 
-  function init() {
-    console.log("sidebar init");
+  var isPositionLeft, openStatus, body, toggleButton;
+  var positionProperty = 'sidebar_position', openStatusProperty = 'sidebar_opened_status';
+
+
+  /**
+   * Initialize the sidebar position
+   */
+  function initPosition() {
+    isPositionLeft = (settings.getString(positionProperty, 'right') !== 'right');
+    openStatus = (settings.getString(openStatusProperty, 'true') === 'true');
+
+    toggleButton.toggleClass('hidden', !openStatus);
+    body.toggleClass('sidebarleft', isPositionLeft);
+    body.toggleClass('sidebarClosed', !openStatus);
   }
 
-  self.init = init;
 
-  return self;
+  /**
+   * Set the sidebar position based on the settings
+   */
+  function toggleSidebar() {
+    toggleButton.toggleClass('hidden', openStatus);
+    body.toggleClass('sidebarClosed', openStatus);
+
+    openStatus = !openStatus;
+    settings.setString(openStatusProperty, openStatus);
+  }
+
+  /**
+   * Return open status of the sidebar
+   */
+  function isOpen(){
+    return openStatus;
+  }
+
+
+  function isSideBarRight(){
+    return !isPositionLeft;
+  }
+
+
+  function setSideBarPosition(position){
+    position = position.toLowerCase();
+    if(!!position.match(/^(left|right)$/)) {
+      isPositionLeft = (position === 'left');
+      settings.setString(positionProperty, position);
+    }
+    initPosition();
+  }
+
+
+  function init() {
+    body = $('body');
+    toggleButton = $('a.toggleSidebar', '.rightBottom');
+
+    initPosition();
+
+    $('.rightBottom').delegate('a.toggleSidebar', 'click', toggleSidebar);
+    $('#sidebar').addClass('slideTransition');
+  }
+
+  return {
+    'init': init,
+    'isOpen': isOpen,
+    'isSideBarRight': isSideBarRight,
+    'setSideBarPosition': setSideBarPosition,
+    'initPosition': initPosition,
+    'toggleSidebar': toggleSidebar
+  };
 
 });
