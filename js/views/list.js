4b8962e2153ff0c6f2d552dfd062fa742f070e92
Aditya Y <aditya@netroy.in>
2012-03-18 01:17:47 +0530

    list partial added as templete to the listview

--- js/views/list.js
+++ js/views/list.js
@@ -4,7 +4,7 @@ define('views/list', ['libs/jquery', 'views/base', 'models/list'], function($, B
 
   var ListView = BaseView.extend({
 
-    'el': '#lists',
+    'template': '<li<%= (inbox == 1)?" class=\'inbox\'":""%>><label><%=name%></label><span class="taskCount"><%=task_count%></span><a class="share"></a><a class="delete"></a><a class="edit"></a><a class="save"></a></li>',
 
     'initialize': function() {
       return BaseView.prototype.initialize(this, arguments);
