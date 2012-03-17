3790e9f967c92bc25515c551125a2f7dda3708cf
Aditya Y <aditya@netroy.in>
2012-03-18 01:18:29 +0530

    forgot to add .apply to the superclass constructor .. fixed

--- js/views/list.js
+++ js/views/list.js
@@ -7,7 +7,7 @@ define('views/list', ['libs/jquery', 'views/base', 'models/list'], function($, B
     'template': '<li<%= (inbox == 1)?" class=\'inbox\'":""%>><label><%=name%></label><span class="taskCount"><%=task_count%></span><a class="share"></a><a class="delete"></a><a class="edit"></a><a class="save"></a></li>',
 
     'initialize': function() {
-      return BaseView.prototype.initialize(this, arguments);
+      return BaseView.prototype.initialize.apply(this, arguments);
     }
 
   });
