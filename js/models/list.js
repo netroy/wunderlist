4432016f5fc77180c07cf95c46ea968e5adf6bde
Aditya Y <aditya@netroy.in>
2012-03-18 01:21:33 +0530

    added list defaults & removed debug code

--- js/models/list.js
+++ js/models/list.js
@@ -1,9 +1,17 @@
 define('models/list', ['models/base'], function(BaseModel) {
 
-  //"use strict";
+  "use strict";
 
-  //var
-  window.ListModel = BaseModel.extend({
+  var ListModel = BaseModel.extend({
+    
+    'defaults': {
+      'online_id': 0,
+      'position': 0,
+      'deleted': 0,
+      'shared': 0,
+      'task_count': 0,
+      'inbox': 0
+    },
 
     'initialize': function() {
       return BaseModel.prototype.initialize(this, arguments);
