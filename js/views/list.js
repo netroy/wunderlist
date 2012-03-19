4a165f094d28863243f3a3ac2e0d2f0a0d08b708
Aditya Y <aditya@netroy.in>
2012-03-19 10:06:54 +0530

    use translations if available for the list names too

--- js/views/list.js
+++ js/views/list.js
@@ -1,13 +1,25 @@
-define('views/list', ['libs/jquery', 'views/base', 'models/list'], function($, BaseView, ListModel) {
+define('views/list',
+      ['libs/jquery', 'views/base', 'models/list', 'helpers/language'],
+      function($, BaseView, ListModel, language, undefined) {
 
   "use strict";
 
+  var BaseViewPrototype = BaseView.prototype;
   var ListView = BaseView.extend({
 
-    'template': '<li<%= (inbox == 1)?" class=\'inbox\'":""%>><label><%=name%></label><span class="taskCount"><%=task_count%></span><a class="share"></a><a class="delete"></a><a class="edit"></a><a class="save"></a></li>',
+    'template': '<li<%= (inbox == 1)?" class=\'inbox\'":""%>><label><%=label%></label><span class="taskCount"><%=task_count%></span><a class="share"></a><a class="delete"></a><a class="edit"></a><a class="save"></a></li>',
 
     'initialize': function() {
-      return BaseView.prototype.initialize.apply(this, arguments);
+      return BaseViewPrototype.initialize.apply(this, arguments);
+    },
+
+    'render': function() {
+      var self = this,
+          model = self.model,
+          name = self.model.get('name'),
+          label = language.data[name.toLowerCase()] || name;
+      self.model.set('label', label);
+      return BaseViewPrototype.render.apply(this, arguments);
     }
 
   });
