b787ec83c11881c9a7e69bc6566998af48664e6e
Aditya Y <aditya@netroy.in>
2012-03-19 09:29:46 +0530

    Menu now uses views peoperly & re-render on language change automatically

--- js/frontend/menu.js
+++ js/frontend/menu.js
@@ -1,62 +1,84 @@
 /*global define:false */
 define('frontend/menu',
-      ['libs/jquery', 'views/base', 'helpers/language', 'helpers/settings', 'backend/account', 'backend/sync', 'frontend/dialogs'],
-      function($, BaseView, language, settings, account, sync, dialogs, undefined) {
+      ['libs/jquery',
+       'views/base', 'models/base', 'helpers/language', 'helpers/settings',
+       'backend/account', 'backend/sync', 'frontend/dialogs'],
+      function($, BaseView, BaseModel, language, settings, account, sync, dialogs, undefined) {
 
   "use strict";
 
-  function openURL(url) {
-    return function() {
-      window.open(url);
-    };
-  }
-
   var Item = BaseView.extend({
+
+    tagName: 'li',
 
     initialize: function(label, handler, cssClass) {
       var self = this;
+      BaseView.prototype.initialize.apply(self, []);
 
-      self.label = label || "";
-      self.elem = $("<li/>").html(label);
+      self.model = self.model || new BaseModel({
+        "label": label
+      });
+
+      self.label = $('<span/>');
+      self.el.append(self.label);
+
+      // On label change the item should re-render
+      self.model.bind('change:label', self.render);
+
+      self.handler = handler;
       self.cssClass = cssClass;
-      if(typeof cssClass !== 'undefined') {
-        self.elem.addClass(cssClass);
-      }
-
-      // BaseView.prototype.initialize.apply(self, arguments);
-
       if(typeof handler === 'function') {
-        self.elem.click(handler);
-      } else if(typeof handler === 'string') {
-        self.elem.html("<a href=" + handler + " target='_blank'>" + label + "</a>");
+        self.el.click(handler);
       }
-
     },
 
     addItem: function(label, handler, cssClass) {
       var self = this;
       var item = new Item(label, handler, cssClass);
-      if(typeof self.submenu === 'undefined'){
+      if(typeof self.submenu === 'undefined') {
         self.submenu = $('<ul/>').addClass(self.cssClass);
-        self.elem.append(self.submenu);
+        self.el.append(self.submenu);
       }
-      this.submenu.append(item.elem);
+      this.submenu.append(item.render().el);
       return item;
     },
 
-    addSeparatorItem: function() {
+    addSeparator: function() {
       this.submenu.append("<li class='separator' />");
     },
 
     clear: function() {
       this.submenu.remove();
-      this.elem.remove();
+      this.el.remove();
     },
 
     setLabel: function(label) {
-      this.label = label;
-      this.elem.html(label || "");
+      this.model.set('label', label);
+    },
+
+    render: function() {
+      var self = this,
+          el = self.el,
+          label = self.label,
+          model = self.model,
+          cssClass = self.cssClass,
+          handler = self.handler,
+          text = model.get('label');
+
+      text = language.data[text] || text;
+      if(typeof cssClass !== 'undefined') {
+        el.addClass(cssClass);
+      }
+
+      if(typeof handler === 'string') {
+        el.html("<a href=" + handler + " target='_blank'>" + text + "</a>");
+      } else {
+        label.html(text);
+      }
+
+      return self;
     }
+
   });
 
 
@@ -66,101 +88,99 @@ define('frontend/menu',
     var menu = new Item();
     menu.submenu = $('#bottomBar ul.menu');
 
-
-    var accountMenuItem   = menu.addItem(language.data.account,  undefined, 'account');
-    var settingsMenuItem  = menu.addItem(language.data.settings, undefined, 'settings');
-    var downloadsMenuItem = menu.addItem(language.data.downloads,undefined, 'downloads');
-    var aboutUsMenuItem   = menu.addItem(language.data.about_us, undefined, 'aboutus');
+    var accountMenuItem   = menu.addItem('account',  undefined, 'account');
+    var settingsMenuItem  = menu.addItem('settings', undefined, 'settings');
+    var downloadsMenuItem = menu.addItem('downloads',undefined, 'downloads');
+    var aboutUsMenuItem   = menu.addItem('about_us', undefined, 'aboutus');
 
     /*
      * Accounts
      */
-    accountMenuItem.addItem(language.data.invitation, account.showInviteDialog);
+    accountMenuItem.addItem('invitation', account.showInviteDialog);
     if (account.isLoggedIn()) {
-      accountMenuItem.addItem(language.data.change_login_data, account.editProfile);
-      accountMenuItem.addItem(language.data.delete_account, account.deleteAccount);
-      accountMenuItem.addSeparatorItem();
+      accountMenuItem.addItem('change_login_data', account.editProfile);
+      accountMenuItem.addItem('delete_account', account.deleteAccount);
+      accountMenuItem.addSeparator();
       var logOutParent = (settings.os === "web") ? menu : accountMenuItem;
-      logOutParent.addItem(language.data.logout, function() {
+      logOutParent.addItem('logout', function() {
         sync.fireSync(true);
       }, 'logout');
     } else {
-      accountMenuItem.addItem(language.data.sign_in, function() {
-        //helpers.dialogs.closeEveryone();
-        account.showRegisterDialog();
-      });
+      accountMenuItem.addItem('sign_in', account.showRegisterDialog);
     }
 
     // Language Menu
-    var languageMenuItem = settingsMenuItem.addItem(language.data.language, undefined, 'language');
+    var languageMenuItem = settingsMenuItem.addItem('language', undefined, 'language');
     var languages = language.availableLang, languageItem;
     $.each(languages, function(code) {
       languageItem = languageMenuItem.addItem(languages[code].translation);
-      languageItem.elem.attr("class", code);
+      languageItem.el.attr("class", code);
     });
-    languageMenuItem.elem.delegate('li', 'click', function(e) {
-      language.setLanguage(e.target.className);
+    languageMenuItem.el.delegate('li', 'click', function(e) {
+      language.setLanguage(e.currentTarget.className);
     });
 
 
-    settingsMenuItem.addSeparatorItem();
-    settingsMenuItem.addItem(language.data.add_item_method, dialogs.openSelectAddItemMethodDialog);
-    settingsMenuItem.addItem(language.data.switchdateformat, dialogs.openSwitchDateFormatDialog);
-    settingsMenuItem.addItem(language.data.sidebar_position, dialogs.openSidebarPositionDialog);
-    settingsMenuItem.addItem(language.data.delete_prompt_menu, dialogs.openDeletePromptDialog);
+    settingsMenuItem.addSeparator();
+    settingsMenuItem.addItem('add_item_method', dialogs.openSelectAddItemMethodDialog);
+    settingsMenuItem.addItem('switchdateformat', dialogs.openSwitchDateFormatDialog);
+    settingsMenuItem.addItem('sidebar_position', dialogs.openSidebarPositionDialog);
+    settingsMenuItem.addItem('delete_prompt_menu', dialogs.openDeletePromptDialog);
 
     var isNaturalDateRecognitionEnabled = settings.getInt('enable_natural_date_recognition', 0);
-    var enableNaturalDateRecognitionMenuString = language.data.enable_natural_date_recognition;
+    var enableNaturalDateRecognitionMenuString = 'enable_natural_date_recognition';
     var enableNaturalDateRecognitionMenuItem;
     if (isNaturalDateRecognitionEnabled === 1) {
-      enableNaturalDateRecognitionMenuString = language.data.disable_natural_date_recognition;
+      enableNaturalDateRecognitionMenuString = 'disable_natural_date_recognition';
     }
     enableNaturalDateRecognitionMenuItem = settingsMenuItem.addItem(enableNaturalDateRecognitionMenuString, function () {
       var isNaturalDateRecognitionEnabled = settings.getInt('enable_natural_date_recognition', 0);
       if (isNaturalDateRecognitionEnabled === 1) {
         settings.setInt('enable_natural_date_recognition', 0);
-        enableNaturalDateRecognitionMenuItem.setLabel(language.data.enable_natural_date_recognition);
+        enableNaturalDateRecognitionMenuItem.setLabel('enable_natural_date_recognition');
       } else {
         settings.setInt('enable_natural_date_recognition', 1);
-        enableNaturalDateRecognitionMenuItem.setLabel(language.data.disable_natural_date_recognition);
+        enableNaturalDateRecognitionMenuItem.setLabel('disable_natural_date_recognition');
       }
     });
-    settingsMenuItem.addSeparatorItem();
+    settingsMenuItem.addSeparator();
 
     // Reset Window Positions
-    //settingsMenuItem.addItem(language.data.reset_window_size, resetWindowSize);
-    //settingsMenuItem.addItem(language.data.reset_note_window, resetNotesWindow);
+    settingsMenuItem.addItem('reset_window_size', undefined);
+    settingsMenuItem.addItem('reset_note_window', undefined);
 
     // Create Tutorials
-    //settingsMenuItem.addItem(language.data.create_tutorials, database.recreateTuts);
+    //settingsMenuItem.addItem('create_tutorials', database.recreateTuts);
 
     /*
      * About Wunderlist
      */
-    aboutUsMenuItem.addItem(language.data.knowledge_base, 'http://support.6wunderkinder.com/kb');
-    //aboutUsMenuItem.addItem(language.data.privacy_policy, 'http://www.6wunderkinder.com');
-    aboutUsMenuItem.addItem(language.data.wunderkinder_tw, 'http://www.twitter.com/6Wunderkinder');
-    aboutUsMenuItem.addItem(language.data.wunderkinder_fb, 'http://www.facebook.com/6Wunderkinder');
-    aboutUsMenuItem.addSeparatorItem();
-    aboutUsMenuItem.addItem(language.data.changelog, 'http://www.6wunderkinder.com/wunderlist/changelog');
-    aboutUsMenuItem.addItem(language.data.backgrounds, dialogs.openBackgroundsDialog);
-    aboutUsMenuItem.addItem(language.data.about_wunderlist, dialogs.openCreditsDialog);
-    aboutUsMenuItem.addItem(language.data.about_wunderkinder, 'http://www.6wunderkinder.com/');
+
+    aboutUsMenuItem.addItem('knowledge_base', 'http://support.6wunderkinder.com/kb');
+    //aboutUsMenuItem.addItem('privacy_policy', 'http://www.6wunderkinder.com');
+    aboutUsMenuItem.addItem('wunderkinder_tw', 'http://www.twitter.com/6Wunderkinder');
+    aboutUsMenuItem.addItem('wunderkinder_fb', 'http://www.facebook.com/6Wunderkinder');
+    aboutUsMenuItem.addSeparator();
+    aboutUsMenuItem.addItem('changelog', 'http://www.6wunderkinder.com/wunderlist/changelog');
+    aboutUsMenuItem.addItem('backgrounds', dialogs.openBackgroundsDialog);
+    aboutUsMenuItem.addItem('about_wunderlist', dialogs.openCreditsDialog);
+    aboutUsMenuItem.addItem('about_wunderkinder', 'http://www.6wunderkinder.com/');
+
 
     /*
      * Downloads
      */
-    downloadsMenuItem.addItem('iPhone',  'http://itunes.apple.com/us/app/wunderlist-to-do-listen/id406644151');
-    downloadsMenuItem.addItem('iPad',    'http://itunes.apple.com/us/app/wunderlist-hd/id420670429');
-    downloadsMenuItem.addItem('Android', 'http://market.android.com/details?id=com.wunderkinder.wunderlistandroid');
+    downloadsMenuItem.addItem('iphone',    'http://itunes.apple.com/us/app/wunderlist-to-do-listen/id406644151');
+    downloadsMenuItem.addItem('ipad',      'http://itunes.apple.com/us/app/wunderlist-hd/id420670429');
+    downloadsMenuItem.addItem('android',   'http://market.android.com/details?id=com.wunderkinder.wunderlistandroid');
     if (settings.os !== 'darwin') {
-      downloadsMenuItem.addItem('Mac OSX', 'http://www.6wunderkinder.com/wunderlist/');
+      downloadsMenuItem.addItem('macosx',  'http://www.6wunderkinder.com/wunderlist/');
     }
     if (settings.os !== 'windows') {
-      downloadsMenuItem.addItem('Windows', 'http://www.6wunderkinder.com/wunderlist/');
+      downloadsMenuItem.addItem('windows', 'http://www.6wunderkinder.com/wunderlist/');
     }
     if (settings.os !== 'linux') {
-      downloadsMenuItem.addItem('Linux',   'http://www.6wunderkinder.com/wunderlist/');
+      downloadsMenuItem.addItem('linux',   'http://www.6wunderkinder.com/wunderlist/');
     }
   }
 
